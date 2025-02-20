02de8911277724b252f9874883efa6b658ebdbcd
MeEatDimsum <cleavon.hezekia@gmail.com>
2025-02-20 04:34:11 -0500

    menambahkan fungsi pengurangan, perkalian, dan pembagian

---
+++ main2.py
@@ -0,0 +1,27 @@
+#tolong lengkapi fungsi python berikut untuk membuat kalkulator sederhana
+#buat dengan format yang sama
+
+
+def penambahan(a,b):
+	total = a+b
+	return(total)
+
+def pengurangan(a,b):
+	total = a-b
+	return(total)
+
+def perkalian(a,b):
+	total = a*b 
+	return(total)
+
+def pembagian(a,b):
+	total = a/b 
+	return(total)
+
+def main():
+	print(penambahan(10,5))
+	print(pengurangan(10,5))
+	print(perkalian(10,5))
+	print(pembagian(10,5))
+
+main()
